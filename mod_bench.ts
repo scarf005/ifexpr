0c205661a6a81a2a03eb819d8be9a6b81980e9b7
scarf <greenscarf005@gmail.com>
2023-02-10 21:58:19 +0900

    docs(README): update benchmark

--- mod_bench.ts
+++ mod_bench.ts
@@ -4,15 +4,15 @@ Deno.bench(function nativeIf() {
   const guard = (a: number | "first") => {
     if (a === "first") {
       return "this is first"
-    } else if (a > 1000) {
-      return "this is bigger than 1000"
-    } else if (a === 1000) {
-      return "this is 1000"
+    } else if (a > 1500) {
+      return "this is bigger than 1500"
+    } else if (a === 1500) {
+      return "this is 1500"
     } else {
-      return "this is smaller than 1000"
+      return "this is smaller than 1500"
     }
   }
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 3000; i++) {
     guard(i)
   }
 })
@@ -21,11 +21,11 @@ Deno.bench(function exprIf() {
   const guard = (a: number | "first") =>
     ifexpr(
       [a === "first", "this is first"],
-      [a > 1000, "this is bigger than 1000"],
-      [a === 1000, "this is 1000"],
-      "this is smaller than 1000",
+      [a > 1500, "this is bigger than 1500"],
+      [a === 1500, "this is 1500"],
+      "this is smaller than 1500",
     )
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 3000; i++) {
     guard(i)
   }
 })
